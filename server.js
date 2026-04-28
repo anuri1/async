c7e8e8e4cc4277a2e33965c96d1681de73aa924f
NazarovReal <nazarovreal5@gmail.com>
2026-04-28 18:08:10 +0500

    4 задание

--- server.js
+++ server.js
@@ -9,13 +9,14 @@ async function run() {
     const orgOgrns = await sendRequest(API.organizationList);
     const ogrns = orgOgrns.join(",");
 
-    const requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+    const [requisites, analytics, buh] = await Promise.all([
+        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`),
+        sendRequest(`/api3/analitics?ogrn=${ogrns}`),
+        sendRequest(`${API.buhForms}?ogrn=${ogrns}`),
+    ]);
+
     const orgsMap = reqsToMap(requisites);
-
-    const analytics = await sendRequest(`/api3/analitics?ogrn=${ogrns}`);
     addInOrgsMap(orgsMap, analytics, "analytics");
-
-    const buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
     addInOrgsMap(orgsMap, buh, "buhForms");
 
     render(orgsMap, orgOgrns);
